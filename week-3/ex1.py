4369a1ef26b1571d139560c485c57e6f07404499
surasal2122 <yithohunsursakdi@gmail.com>
2025-07-17 12:11:21 +0700

    week_3 commit

---
+++ week-3/ex1.py
@@ -0,0 +1,10 @@
+decimal = input("input decimal heve:")
+decimal = int(decimal) 
+
+binary = bin(decimal)
+hex = hex(decimal)
+oct = oct(decimal)
+
+print("this is binary",binary)
+print("this is  hex",hex)
+print("this  is  oct",oct)
